fe001e4346d2871a30da097fe173cf44767b58f2
yasmin <zhamborova.y@gmail.com>
2020-11-19 20:50:37 +0300

    everything workssss

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/cs4550-f20-client-angular-zhamborova'));
+app.use(express.static(__dirname + '/dist/cs4550-f20-client-angular-jannunzi'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/cs4550-f20-client-angular-zhamborova/src/index.html'));});
+    '/dist/cs4550-f20-client-angular-jannunzi/index.html'));});
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
 
 
