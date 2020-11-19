b0df4fcec0e77c14996deca6dbfbdf56b2d4b8cb
yasmin <zhamborova.y@gmail.com>
2020-11-19 17:04:05 +0300

    s

--- src/services/quiz-service.ts
+++ src/services/quiz-service.ts
@@ -5,6 +5,7 @@ import {Injectable} from '@angular/core';
 export class QuizzesServiceClient {
   findAllQuizzes = () =>
     fetch('https://node-heroku-zhamborova.herokuapp.com/api/quizzes')
+      ///dt
       .then(response => response.json())
   findQuizById = (qid) =>
     fetch(`https://node-heroku-zhamborova.herokuapp.com/api/quizzes/${qid}`)
