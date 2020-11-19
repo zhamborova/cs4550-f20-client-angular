b03177e5583d14ed9e926b99a451d42d8ba59b37
yasmin <zhamborova.y@gmail.com>
2020-11-19 20:28:25 +0300

    ser

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/cs4550-f20-client-angular-zhamborova'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/src/index.html'));});
+    '/dist/cs4550-f20-client-angular-zhamborova/src/index.html'));});
 
 app.listen(process.env.PORT || 8080);
+
+
